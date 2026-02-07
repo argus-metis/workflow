ec31a52f3eb78e6d59f227dffbb0a9c530ad1edb
Peter Wielander <mittgfu@gmail.com>
2026-02-06 16:02:08 -0800

    Updat
    Signed-off-by: Peter Wielander <mittgfu@gmail.com>

--- packages/world-vercel/src/utils.ts
+++ packages/world-vercel/src/utils.ts
@@ -26,7 +26,7 @@ import { version } from './version.js';
  * Example: 'https://workflow-server-git-branch-name.vercel.sh'
  */
 const WORKFLOW_SERVER_URL_OVERRIDE =
-  'https://workflow-server-5vv04oiox.vercel.sh';
+  'https://workflow-server-h9nmj88gs.vercel.sh';
 
 export interface APIConfig {
   token?: string;
